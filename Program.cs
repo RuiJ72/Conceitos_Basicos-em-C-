311e1a69472ba8478cee5793f9dc19b51fd47b64
RuiJ72 <rlagos900@gmail.com>
2022-05-08 02:08:54 -0300

    Adicionadas Aulas 1 e 2

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace Curso
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            string Condicao;
+
+            Console.WriteLine("Olá");
+            Console.WriteLine("Como está");
+            Condicao = Console.ReadLine();
+
+            Console.WriteLine("A sua condição é : " + Condicao);
+
+
+
+            Console.ReadKey();
+        }
+        
+     }
+
+ }
+
